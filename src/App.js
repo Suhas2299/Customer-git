5a44cc0cdbafbe943aa8b0f7a9234534dc18af38
Suhas2299 <johndoe@example.com>
2021-07-12 09:34:24 +0530

    updated1

--- src/App.js
+++ src/App.js
@@ -8,63 +8,24 @@ const initState = {
 };
 
 const PROGRESS_ACTION_TYPE = "PROGRESS_ACTION_TYPE";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { CustomerUpsert } from "./pages/CustomerUpsert";
+import { CustomerList } from "./pages/CustomerList";
 
-const CUSTOMER_CREATE_ACTION_TYPE = "CUSTOMER_CREATE_ACTION_TYPE";
+function App() {
+  return (
+    <Router>
+      <Route exact path="/customer-upsert" component={CustomerUpsert} />
 
-const CUSTOMER_GET_ALL_ACTION_TYPE = "CUSTOMER_GET_ALL_ACTION_TYPE";
+      <Route exact path="/" component={CustomerUpsert} />
+      <Route exact path="/customer-list" component={CustomerList} />
 
-// const CUSTOMER_UPDATE_RENDER_ACTION_TYPE = "CUSTOMER_UPDATE_RENDER_ACTION_TYPE";
-
-export const getAllCustomerAction = () => {
-  return async (dispatch) => {
-    // API CALL
-    const url = `http://localhost:8080/customer/allcustomer`;
-    const response = await axios.get(url);
-
-    console.log(response);
-
-    // UI UPDATE
-    dispatch({ type: "CUSTOMER_GET_ALL_ACTION_TYPE", payload: response.data });
-  };
-};
-
-// export const updateRenderAction = (payload) => {
-
-//   return { type: CUSTOMER_UPDATE_RENDER_ACTION_TYPE, payload: payload };
-// };
-
-export const cretaeCustomerAction = (payload) => {
-  return async (dispatch) => {
-    const url = `http://localhost:8080/customer/customers`;
-    await axios.post(url, payload);
-
-    // updating the ui.
-    dispatch({ type: PROGRESS_ACTION_TYPE, payload: true });
-
-    // after 5 second PROGRESS :: FALSE AGAIN
-    setTimeout(() => {
-      dispatch({ type: PROGRESS_ACTION_TYPE, payload: false });
-    }, 5000);
-  };
-};
-
-function CustomerReducer(state = initState, action) {
-  switch (action.type) {
-    case CUSTOMER_GET_ALL_ACTION_TYPE:
-      return { ...state, customerList: action.payload };
-
-    case PROGRESS_ACTION_TYPE:
-      return { ...state, progress: action.payload };
-
-    // case CUSTOMER_UPDATE_RENDER_ACTION_TYPE:
-    //   // 6
-    //   return { ...state, uref: action.payload };
-
-    default:
-      return state;
-  }
+      {/* <Route exact path="/customer-upsert" component={CustomerById} />
+      <Route exact path="/" component={CustomerById} /> */}
+    </Router>
+  );
 }
 
-// MIDDLEWARE FOR THE ASYNC OPOERATION
-const store = createStore(CustomerReducer, applyMiddleware(thunk));
-export { store };
+export default App;
--- src/pages/CustomerList.js
+++ src/pages/CustomerList.js
@@ -1,173 +1,73 @@
-import { useEffect, useRef } from "react";
-import { useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { cretaeCustomerAction } from "../redux/store";
+import { getAllCustomerAction, updateRenderAction } from "../redux/store";
 import { AppNav } from "./AppNav";
+import { useHistory } from "react-router-dom";
 
-export const CustomerUpsert = () => {
+export const CustomerList = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
+  const history = useHistory();
 
-  const formEl = useRef();
+  useEffect(() => {
+    dispatch(getAllCustomerAction());
+  }, []);
 
-  const [firstName, setFirstName] = useState("");
-  const [middleName, setMiddleName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [addharNumber, setAddharNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobileNumber, setMobileNumber] = useState("");
-  const [gender, setGender] = useState("");
+  // const updateRecord = (item) => {
+  //   console.log("Update Record", item);
 
-  const updateFirstName = (e) => setFirstName(e.target.value);
-  const updateMiddleName = (e) => setMiddleName(e.target.value);
-  const updateLastName = (e) => setLastName(e.target.value);
-  const updateAddharNumber = (e) => setAddharNumber(e.target.value);
-  const updateEmail = (e) => setEmail(e.target.value);
-  const updateMobileNumber = (e) => {
-    console.log(e.target.value);
-    const numericValue = e.target.value.replace(/[^\d]/gi, "");
-    setMobileNumber(numericValue);
-  };
+  //   // 3 :: updating the store
+  //   dispatch(updateRenderAction(item));
 
-  const updateGender = (e) => setGender(e.target.value);
-
-  const addNewCustomer = (e) => {
-    // console.log(formEl.current);
-    // console.log(formEl.current.checkValidity());
-
-    e.preventDefault();
-
-    const isFormValid = formEl.current.checkValidity();
-    if (isFormValid) {
-      dispatch(
-        cretaeCustomerAction({
-          firstName,
-          middleName,
-          lastName,
-          addharNumber,
-          email,
-          mobileNumber,
-          gender,
-        })
-      );
-    } else {
-      e.stopPropagation();
-      formEl.current.classList.add("was-validated");
-    }
-  };
+  //   // navigateing to the page
+  //   history.push("/customer-upsert");
+  // };
 
   return (
     <div>
       <AppNav />
-      <div className="alert alert-secondary">
-        <h3>Customer Register</h3>
+      <div className="alert alert-secondary mb-0">
+        <h3>Customer List</h3>
       </div>
 
-      {state.progress && (
-        <div className="mx-4 alert alert-success">
-          Customer added Successfully
-        </div>
-      )}
-
-      <form ref={formEl} className="mx-4 needs-validation" noValidate>
-        <div>
-          <input
-            type="text"
-            value={firstName}
-            onChange={updateFirstName}
-            className="form-control form-control-lg mb-1"
-            placeholder="Enter First Name"
-            minLength="3"
-            maxLength="30"
-            required
-          />
-        </div>
-
-        <div>
-          <input
-            type="text"
-            value={middleName}
-            onChange={updateMiddleName}
-            className="form-control form-control-lg mb-1"
-            placeholder="Enter Middle Name"
-            minLength="3"
-            maxLength="30"
-            required
-          />
-        </div>
-
-        <div>
-          <input
-            type="text"
-            value={lastName}
-            onChange={updateLastName}
-            className="form-control form-control-lg mb-1"
-            placeholder="Enter Last Name"
-            minLength="3"
-            maxLength="30"
-            required
-          />
-        </div>
-
-        <div>
-          <input
-            type="text"
-            value={addharNumber}
-            onChange={updateAddharNumber}
-            className="form-control form-control-lg mb-1"
-            placeholder="Enter Adhar Number"
-            maxLength="12"
-            required
-          />
-        </div>
-
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={updateEmail}
-            className="form-control form-control-lg mb-1"
-            placeholder="Enter Email"
-            minLength="3"
-            maxLength="30"
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            value={mobileNumber}
-            onChange={updateMobileNumber}
-            className="form-control form-control-lg mb-1"
-            placeholder="Enter Mobile Number"
-            minLength="3"
-            maxLength="30"
-            required
-          />
-        </div>
-
-        <div>
-          <input
-            type="text"
-            value={gender}
-            onChange={updateGender}
-            className="form-control form-control-lg mb-1"
-            placeholder="Enter Gender"
-            minLength="3"
-            maxLength="6"
-            required
-          />
-        </div>
-
-        <div>
-          <input
-            type="button"
-            onClick={addNewCustomer}
-            value="Add Customer"
-            className="btn btn-lg btn-success w-100"
-          />
-        </div>
-      </form>
-    </div> 
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">addharNumber</th>
+            <th scope="col">firstName</th>
+            <th scope="col">middleName</th>
+            <th scope="col">lastName</th>
+            <th scope="col">mobileNumber</th>
+            <th scope="col">email</th>
+            <th scope="col">gender</th>
+            <th scope="col"> action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.customerList.map((item, index) => (
+            <tr key={index}>
+              <th scope="row">{item.customerId}</th>
+              <td>{item.addharNumber}</td>
+              <td>{item.firstName}</td>
+              <td>{item.middleName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.mobileNumber}</td>
+              <td>{item.email}</td>
+              <td>{item.gender}</td>
+              <td>
+                <input
+                  type="button"
+                  value="Update"
+                  className="btn btn-outline-success btn-sm  mr-1"
+                  // onClick={updateRecord} :1
+                  // onClick={() => updateRecord(item)}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
